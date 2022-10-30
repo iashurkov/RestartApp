53a1376fb67398c6d8d15af2538a2a9cdd57abca
iashurkov <iashurkov@uma.tech>
2022-10-30 15:04:01 +0300

    Initial Commit

---
+++ RestartApp_SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RestartApp_SwiftUI
+//
+//  Created by Igor Ashurkov on 30.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RestartApp_SwiftUI/RestartApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp_SwiftUIApp.swift
+//  RestartApp_SwiftUI
+//
+//  Created by Igor Ashurkov on 30.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
